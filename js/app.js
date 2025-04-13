caded9e79ef2d48bf896b6dc0364581a18c3ce9a
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-13 16:49:31 +0200

    js intro

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log('JavaScript');
+console.log('jest');
+console.log('COOL');
+
+const str = 'wartość typu string'
+const sum = 1 + 2 // komentarz w JS
+
+
+console.log('wyświetlam', str);
+console.log('wyświetlam również liczbę', sum);
+
+alert('JS działa!')

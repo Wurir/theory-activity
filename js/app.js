510f6229b2db49e49990e962b55d2d3e159606e9
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-14 17:30:29 +0200

    konkantenacja

--- js/app.js
+++ js/app.js
@@ -1,20 +1,11 @@
-const res1 = 2 + 4; 
-// 6
-const res2 = res1 / 2; 
-// 3
-const res3 = res2 * res2; 
-// 9
-const res4 = res3 / 2; 
-// 4.5
-const res5 = res4 % 2; 
-// 0.5
-let res6 = 1; res6++; 
-// 2
-let res7 = 2; res7--; 
-// 1
-const res8 = 2 + '1'; 
-// '21'
+const x = 1
+const y = '2'
 
-console.log(
-res1, res2, res3, res4, res5, res6, res7, res8
-);
+const result1 = x + y
+console.log(result1, typeof result1);
+// '12' string
+
+const z  = parseInt(y)
+const result2 = x + y
+console.log(result2, typeof result2);
+// 3 number

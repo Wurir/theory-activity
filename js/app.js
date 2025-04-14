78d4296e9d66bc8fe61d36e6947d398cf90ab7e8
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-14 18:02:12 +0200

    operatory logiczne

--- js/app.js
+++ js/app.js
@@ -1,27 +1,15 @@
-console.log('0' == 0); 
-// true,
-// bo '0' => 0, 0 === 0
+const x = 5
+const y = 6
 
-console.log(0 == false); 
+console.log( x < y);
 // true
-// bo false => 0, 0 === 0
 
-console.log(false == '0'); 
-// true
-// bo false => 0 & '0' => 0, 0 === 0
-// jeśli mamy luźne (loose) porównanie tj. ==
-// JS używa niejawnej konwersji, aby wartości
-// sprowadzić do tego samego typu
-// i dopiero potem porównać
-
-console.log('0' === 0); 
+console.log( !(x < y));
 // false
 
-console.log(0 === false); 
+console.log( x < y && x > y);
 // false
 
-console.log(false === '0');
- // false
-// w przypadku tzw. ścisłego (strict) porównania
-// tj. użycia === typ wartości musi się zgadzać
-// jeśli tak nie jest już mamy fałsz
+console.log( x < y || x > y);
+// true
+

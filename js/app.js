b297ebf0982229df1ae5d4e125338e449ed07155
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-14 17:34:09 +0200

    NaN

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
-const x = '1'
+const x = 'a'
 const y = '2'
 
-const result1 = x * y
+const result1 = x / y
 console.log(result1, typeof result1);
 // 2 string
 
-const z  = 'abc'
-const result2 = x * z
-console.log(result2, typeof result2);
-// NaN number
+console.log(result1 === result1);
+// false

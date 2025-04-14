d8584a19d286f34e1f1cca41d3d4aca69da60e53
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-14 17:33:01 +0200

    konkantenacja 2

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-const x = 1
+const x = '1'
 const y = '2'
 
-const result1 = x + y
+const result1 = x * y
 console.log(result1, typeof result1);
-// '12' string
+// 2 string
 
-const z  = parseInt(y)
-const result2 = x + y
+const z  = 'abc'
+const result2 = x * z
 console.log(result2, typeof result2);
-// 3 number
+// NaN number

ee88008614309a7759461a4b79ae0c05532eab3b
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-14 17:27:16 +0200

    operatory arytmetyczne

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
-const txt = '245.65'
-const numInt = parseInt(txt)
-const numFloat = parseFloat(txt)
+const res1 = 2 + 4; 
+// 6
+const res2 = res1 / 2; 
+// 3
+const res3 = res2 * res2; 
+// 9
+const res4 = res3 / 2; 
+// 4.5
+const res5 = res4 % 2; 
+// 0.5
+let res6 = 1; res6++; 
+// 2
+let res7 = 2; res7--; 
+// 1
+const res8 = 2 + '1'; 
+// '21'
 
-// JS nie rozróżnia wartości całkowitych (integer/int) oraz rzeczywistych (float)
-// jednak możemy "obciąć" wartości po przecinku (kropce)
-
-console.log(numInt, typeof numInt, numFloat, typeof numFloat);
-// 245 number 245.65 number
+console.log(
+res1, res2, res3, res4, res5, res6, res7, res8
+);

e5e4a73da9da09ead959651f71b75821e277f598
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-15 17:25:33 +0200

    cdn 2

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
-const x = 5
-const y = 6
+const x = 7
+const y = 1
+const min = 4
+const max = 10
 
-console.log( x < y);
+
+console.log( x >= min && x <= max);
 // true
 
-console.log( !(x < y));
+console.log(y >= min && y <= max);
 // false
 
-console.log( x < y && x > y);
+console.log( x < min || x >= max);
 // false
 
-console.log( x < y || x > y);
+console.log( !(x === y));
 // true
 
-// strona 53

2b0c537e672d03c3c09747533ec5735bfb0a3528
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-15 18:17:26 +0200

    for

--- js/app.js
+++ js/app.js
@@ -1,45 +1,8 @@
-const monthNumber = parseInt(prompt('Podaj numer miesiąca:', 0))
-let monthName
-
-switch(monthNumber) {
-    case 1: 
-        monthName = 'Styczeń'
-        break
-    case 2: 
-        monthName = 'Luty'
-        break
-    case 3: 
-        monthName = 'Marzec'
-        break
-    case 4: 
-        monthName = 'Kwiecień'
-        break
-    case 5: 
-        monthName = 'Maj'
-        break
-    case 6: 
-        monthName = 'Czerwiec'
-        break
-    case 7: 
-        monthName = 'Lipiec'
-        break
-    case 8: 
-        monthName = 'Sierpień'
-        break
-    case 9: 
-        monthName = 'Wrzesień'
-        break
-    case 10: 
-        monthName = 'Październik'
-        break
-    case 11: 
-        monthName = 'Listopad'
-        break
-    case 12: 
-        monthName = 'Grudzień'
-        break
-    default:
-        alert('Podano błędne dane!')
-        break2
+for(let i=0; i<=3;i++){
+    console.log(i);
 }
-console.log(monthName);
+
+for(let i=4; i>=0; i--){
+    console.log(i);
+    
+}

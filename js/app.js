c5f6a7c40a16f754888d554a0274125c32b65777
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-15 18:10:05 +0200

    switch

--- js/app.js
+++ js/app.js
@@ -1,19 +1,45 @@
-const a = 12
-const b = 55
+const monthNumber = parseInt(prompt('Podaj numer miesiąca:', 0))
+let monthName
 
-const info = a > b ? 'A większe' : 'B większe'
-console.log(info);
-
-const diff = a > b ? a - b : b-a
-console.log(diff);
-
-const numsAreEven1 = (a % 2 === 0) && (b % 2 === 0) ? 'yes' : 'no'
-const numsAreEven2 = !(a % 2) && !(b & 2) ? 'yes': 'no'
-
-console.log(numsAreEven1, numsAreEven2);
-// no no
-
-const isEven = a % 2 === 0 ? true : false
-// krocej: const isEven = a % 2 === 0
-console.log(isEven);
-// true
+switch(monthNumber) {
+    case 1: 
+        monthName = 'Styczeń'
+        break
+    case 2: 
+        monthName = 'Luty'
+        break
+    case 3: 
+        monthName = 'Marzec'
+        break
+    case 4: 
+        monthName = 'Kwiecień'
+        break
+    case 5: 
+        monthName = 'Maj'
+        break
+    case 6: 
+        monthName = 'Czerwiec'
+        break
+    case 7: 
+        monthName = 'Lipiec'
+        break
+    case 8: 
+        monthName = 'Sierpień'
+        break
+    case 9: 
+        monthName = 'Wrzesień'
+        break
+    case 10: 
+        monthName = 'Październik'
+        break
+    case 11: 
+        monthName = 'Listopad'
+        break
+    case 12: 
+        monthName = 'Grudzień'
+        break
+    default:
+        alert('Podano błędne dane!')
+        break2
+}
+console.log(monthName);

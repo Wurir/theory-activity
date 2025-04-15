eb87bc85d56d936225511146d94ef7a62f5902c1
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-15 17:50:38 +0200

    operator logiczny i warunkowy skrocony

--- js/app.js
+++ js/app.js
@@ -6,3 +6,9 @@ console.log(info);
 
 const diff = a > b ? a - b : b-a
 console.log(diff);
+
+const numsAreEven1 = (a % 2 === 0) && (b % 2 === 0) ? 'yes' : 'no'
+const numsAreEven2 = !(a % 2) && !(b & 2) ? 'yes': 'no'
+
+console.log(numsAreEven1, numsAreEven2);
+// no no

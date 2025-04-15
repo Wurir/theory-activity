9cef690145259deee7935bd355b263c015ef4bfc
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-15 17:37:39 +0200

    if ... else cdn

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ const userAge = prompt('Ile masz lat?', 0)
 
 const userAgeInt = parseInt(userAge)
 
-if(userAgeInt >= 18) {
-    alert('Jesteś pełnoletni! Gratuluję!')
-} else {
-    const leftAge = 18 -userAge
-    alert('Do pełnoletności brakuję Ci: ' + leftAge + ' lat')
+const isAdult = userAgeInt >= 18
+
+if(!isAdult) {
+
+    const leftAge = 18 - userAge
+
+    alert('Do pełnoletności brakuję Ci: ' + leftAge)
 }

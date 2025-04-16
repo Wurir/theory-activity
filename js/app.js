9376b2cee001705960f7d638c9a10d6ae571d519
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-16 15:47:27 +0200

    while loop cdn

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
-const randomNumber = Math.round(Math.random() * 5)
-// losuje liczbe z przedzialu od 0 do 5
-
-let userNumber
-// deklaruje zmienna, ktora będzie przechowywać liczbę wprowadzoną przez użytkownika
-
-let counter = 0
-// liczba wykonywanych prób
-
-// wykonuj pętle dopóki wylosowa liczba będzie różna od tej wprowadzonej przez uzytkownika
-while(randomNumber !== userNumber) {
-    userNumber = parseInt(prompt('Zgadnij liczbę!'))
-    counter++
+let i = 3
+while(i<=100) {
+    console.log(i);
+    i += 3
 }
 
-alert('Gratuluję! Zgadłeś za '+counter+' razem!')
+let j = 1
+while(j<=100){
+    if (j % 3 === 0) {
+        console.log(j);
+    }
+    j++
+}

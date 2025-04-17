6bd03faf77aba594b930422e161e132205240b19
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-17 14:16:58 +0200

    wyrazenia fn cdn

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
-function getUserInput(messageForUser){
+const fn = function getUserInput(messageForUser){
     const userInput = prompt(messageForUser)
     return userInput
 }
-
-const fn = getUserInput
 
 const inputFromUser = fn('Ile masz lat?')
 console.log(inputFromUser);

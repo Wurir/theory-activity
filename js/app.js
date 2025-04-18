18668dd26f4cba9eb3ebb9d3fc63fc9163e57574
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-18 17:18:10 +0200

    array sort method for numbers

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 const numbers = [111, 12, 2, 1, 201];
 
-numbers.sort()
-console.log(numbers); // [1, 111, 12, 2, 201]
+numbers.sort(function(a, b) {
+    // sortowanie od najmniejszej do najwiekszej wartosci
+    return a - b;
+})
+console.log(numbers);
+
+
+numbers.sort(function(a, b ) {
+    // sortowanie od najwiekszej do najmniejszej wartosci
+    return b - a;
+})
+
+console.log(numbers);

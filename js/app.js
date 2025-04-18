cb57a5c4252428bb7c59442dc312c8547b34aebd
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-18 17:00:13 +0200

    new Array()

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-let arr = []
+let arr = new Array(0)
 // deklaruje pusta tablice
 
-const names = ['Jan', 'Anna', 'Kasia'];
+const names = new Array('Jan', 'Anna', 'Kasia')
 // deklaruje 3 elementowa tablicę
 
 console.log(names[0]); // Jan
@@ -9,7 +9,3 @@ console.log(names[0]); // Jan
 
 console.log(names.length); // 3
 // pobieram ilosc elementow
-
-const lastIndex = names.length - 1;
-console.log(names[lastIndex]); // kasia
-// pobieram ostatni element

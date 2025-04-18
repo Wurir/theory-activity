d169d69fabf144861bbd5a0b20454319a5ebdf57
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-18 17:16:07 +0200

    array.sort - for numbers its complicated

--- js/app.js
+++ js/app.js
@@ -1,17 +1,4 @@
-// deklarujemy zmienna, ktora bedzie przechowywac nasze wylosowane liczby
-const numbers = []
-//tworzymy petle, ktora bedzie sie wykonywac dopoki ilosc elementow w tablicy
-// jest mniejsza niz 5
+const numbers = [111, 12, 2, 1, 201];
 
-while(numbers.length < 5) {
-    // losujemy liczbe od 0 do 10
-    const num = Math.round(Math.random() * 10)
-
-    // sprawdzamy czy wylosowana liczba znajduje sie w tablicy
-    if(!numbers.includes(num)){
-        numbers.push(num)
-        // dodaje wylosowana liczbe do tablicy
-    }
-}
-
-console.log(numbers);
+numbers.sort()
+console.log(numbers); // [1, 111, 12, 2, 201]

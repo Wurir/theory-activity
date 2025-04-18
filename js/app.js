dcc02ac8c606277b236ae79a65e2f52e281fc52e
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-18 15:13:42 +0200

    scope - przeslanianie danych

--- js/app.js
+++ js/app.js
@@ -1,20 +1,13 @@
-// deklaracja zmiennych globalnych
-var firstName = 'Ewa'
-const sum = 12;
-let counter = 1
+// zmienna globalna
+const num = 12;
+console.log(num); // 12
 
-function fn(param1, param2) {
-    // parametry funkcji sa zmiennymi lokalnymi
-    // deklarujemy zmienne lokalne
-    var firstNameLocal = 'Anna'
-    const numLocal = 12;
-    let counterLocal = 2
+
+function fn(){
+    // zmienna lokalna
+    const num = 44
+    console.log(num); // 44
 }
 
-{
-    // deklaracja zmiennej globalnej
-    var firstNameBlock = 'Kasia'
-    // deklaracja zmiennych lokalnych
-    const numBlock = 3;
-    let counterBlock = 12
-}
+fn()
+console.log(num); // 12

e97e262ff4836f0af69941564607ab2aa64a39ca
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-19 17:21:54 +0200

    fn to get evenNumbers from array

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const numbers = [1, 2, 3, 4, 5];
 const sum = getSum(numbers)
-
+const evenNumbers = getEvenNumbers(numbers)
+const sumOfEvenNumbers = getSum(evenNumbers)
 
 function getSum(arr) {
     let sum = 0
@@ -10,4 +11,16 @@ function getSum(arr) {
     return sum
 }
 
+function getEvenNumbers(arr){
+    const evenNumbers = []
+    for(let j=0; j<arr.length; j++){
+        if(arr[j] % 2 === 0){
+            evenNumbers.push(arr[j])
+        }
+    }
+    return evenNumbers
+}
+
 console.log(sum); // 15
+console.log(evenNumbers); //  [2, 4]
+console.log(sumOfEvenNumbers); // 6

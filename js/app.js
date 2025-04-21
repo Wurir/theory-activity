ffc891e39cf8d8eb26909043be5959640ac8d48f
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-21 16:03:11 +0200

    this context

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-const car = {
-    color: 'red',
-    mileage: 1203,
-    horsepower: 540,
-    seatsNumber: 2,
-    getColor: function(){
-        return this.color
-    },
+function showInfo(){
+    console.log(this.text);
 }
 
-car.getSeatsNumber = function(){
-    return this.seatsNumber
+const notice = {
+    text: 'Notice!',
+    showText: showInfo,
 }
 
-console.log(car.getColor(), car.getSeatsNumber());
+const error = {
+    text: 'Error!',
+    showText: showInfo,
+}
 
+notice.showText() // Notice!
+error.showText() // Error!

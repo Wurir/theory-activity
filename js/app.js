aee45d809c98c47b3568b61cc69dcd26e4200876
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-21 20:51:43 +0200

    browser timer using new Date()

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
-const now = new Date()
+setInterval( function() {
+    const now = new Date();
+    const time = getTime(now);
+    document.title = time;
+    console.clear();
+    console.log(time);
+}, 1000);
 
-console.log(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDay(),
-    now.getHours(),
-    now.getMinutes(),
-    now.getSeconds()
-);
+function getTime(date){
+    let time  = startWithZero(date.getHours());
+    time += ':' + startWithZero(date.getMinutes());
+    time += ':' + startWithZero(date.getSeconds());
+    return time
+};
+
+function startWithZero(num){
+    return (num < 10 ? '0' : '') + num;
+}

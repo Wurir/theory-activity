4fdc291612e47403fdd48f40ca2f3cbc6230bb53
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-21 20:34:37 +0200

    new Date()

--- js/app.js
+++ js/app.js
@@ -1,17 +1,10 @@
-const travelPrice = {
-    flight: 591,
-    hotel: 1400,
-    excursions: 421,
-    other: 120
-}
+const now = new Date()
 
-const totalPrice = getTotalPrice(travelPrice)
-console.log(totalPrice); // 2532
-
-function getTotalPrice(priceObject){
-    const values = Object.values(priceObject)
-    const totalPrice = values.reduce(function(acc, price){
-        return acc + price
-    }, 0)
-    return totalPrice
-}
+console.log(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDay(),
+    now.getHours(),
+    now.getMinutes(),
+    now.getSeconds()
+);

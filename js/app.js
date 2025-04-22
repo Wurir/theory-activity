8bcccb30b99ae46f23e46f52f0f5af3ac6d607ad
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-22 13:57:12 +0200

    practice-js-basics-objects /02

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-const user = {
-    firstName: 'Bartek',
-    lastName: 'Bogdanowicz',
-    sex: 'male',
-    age: 31
+const calendarJS = {
+    'ES1': '1997-06',
+    'ES2': '1998-06',
+    'ES3': '1999-12',
+    'ES4': null,
+    'ES5': '2009-12',
+    'ES6': '2015-06',
+    'ES7': '2016-06',
+    'ES8': '2017-06',
+    'ES9': '2018-06',
 }
 
-for(const key in user){
-    console.log(user[key]);
+for(const key in calendarJS){
+    if(calendarJS[key] !== null){
+        console.log(key + ' wydano w terminie ' + calendarJS[key])
+    }else {
+        console.log(key + ' nie zostało wydane')
+    }
 }

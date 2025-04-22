8404de6aa44330af095f9ade299ba05b96f718c9
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-22 14:54:35 +0200

    constructor cdn2

--- js/app.js
+++ js/app.js
@@ -8,7 +8,9 @@ function Student(initFirstName, initLastName){
 }
 
 const student1 = new Student('Marek', 'Nowak')
+student1.firstName = 'Darek'
+student1.lastName = 'Jarek'
 const student2 = new Student('Anna', 'Kowal')
 
 console.log(student1.class, student2.class); // 2B 2B
-student1.showFullName() // Marek Nowak
+student1.showFullName() // Darek Jarek

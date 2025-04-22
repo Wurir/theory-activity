d5d85eb237337a2d4be594aa4c81dba5057ed6cb
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-22 15:10:44 +0200

    prototype

--- js/app.js
+++ js/app.js
@@ -1,22 +1,14 @@
-const studentsNum = parseInt(prompt('Liczba uczniów?'))
-const studentsList = []
-
-for(let i=0; i<studentsNum; i++){
-    const firstName = prompt('Podaj imię (' + i + ')')
-    const lastName = prompt('Podaj nazwisko (' + i + ')')
-
-    const newStudent = new Student(firstName, lastName)
-    studentsList.push(newStudent)
-}
-
-studentsList.forEach(function(item){
-    item.showFullName()
-})
-
 function Student(initFirstName, initLastName){
     this.firstName = initFirstName
     this.lastName = initLastName
-    this.showFullName = function(){
-        console.log(this.firstName, this.lastName);
-    }
 }
+
+Student.prototype.showFullName = function(){
+    console.log(this.firstName, this.lastName);
+}
+
+const student1 = new Student('Anna', 'Kowal')
+student1.showFullName()
+
+const student2 = new Student('Michał', 'Nowak')
+student2.showFullName()

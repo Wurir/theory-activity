15294cd4c50ed55b2ba9f4284dc295673b1b4db2
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-22 14:15:18 +0200

    practice-js-basics-objects /03

--- js/app.js
+++ js/app.js
@@ -1,19 +1,60 @@
-const calendarJS = {
-    'ES1': '1997-06',
-    'ES2': '1998-06',
-    'ES3': '1999-12',
-    'ES4': null,
-    'ES5': '2009-12',
-    'ES6': '2015-06',
-    'ES7': '2016-06',
-    'ES8': '2017-06',
-    'ES9': '2018-06',
+const books = {
+    '978-83-7278-000-3': {
+        author: 'J.K. Rowling',
+        title: {
+            en: 'Harry Potter and the Philosopher\'s Stone',
+            pl: 'Harry Potter i Kamień Filozoficzny',
+        },
+        translator: {
+            en: null,
+            pl: 'Andrzej Polkowski',
+        }
+    },
+    '83-7278-007-2': {
+        author: 'J.K. Rowling',
+        title: {
+            en: 'Harry Potter and the Chamber of Secrets',
+            pl: 'Harry Potter i Komnata Tajemnic ',
+        },
+        translator: {
+            en: null,
+            pl: 'Andrzej Polkowski',
+        }
+    },
 }
 
-for(const key in calendarJS){
-    if(calendarJS[key] !== null){
-        console.log(key + ' wydano w terminie ' + calendarJS[key])
-    }else {
-        console.log(key + ' nie zostało wydane')
+books.getAuthor = function(isbn) {
+    // w tym przypadku this === books
+    
+    if(typeof this[isbn] === 'undefined') {
+        // taki ISBN nie istnieje w moim spisie
+        return null;
     }
+
+    const author = this[isbn]['author'];
+    if(author) {
+        // zwracam informacje o autorze
+        return author;
+    }
+
+    // brak informacji o autorze
+    return false;
 }
+
+books.getTitle = function(isbn, lang) {
+    const title = this[isbn]['title'][lang]
+    return title ? title : null
+}
+
+books.getTranslator = function(isbn, lang) {
+    const translator = this[isbn]['translator'][lang]
+    return translator ? translator : false
+}
+
+
+console.log( books.getAuthor('978-83-7278-000-3') ); // J.K. Rowling
+console.log( books.getAuthor('000-00-0000-000-0') ); // null
+console.log( books.getTitle('978-83-7278-000-3', 'pl') ); // Harry Potter i Kamień Filozoficzny
+console.log( books.getTitle('978-83-7278-000-3', 'en') ); // Harry Potter and the Philosopher's Stone
+console.log( books.getTranslator('83-7278-007-2', 'pl') ); // Andrzej Polkowski
+console.log( books.getTranslator('83-7278-007-2', 'en') ); // false

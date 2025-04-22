57a549eb192975a97ade84b51cbe75229d000702
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-04-22 14:50:20 +0200

    constructors intro

--- js/app.js
+++ js/app.js
@@ -1,31 +1,9 @@
-const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '22',
-        month: '04',
-        year: '1985'
-    }
+function Student(initFirstName, initLastName){
+    this.firstName = initFirstName
+    this.lastName = initLastName
 }
 
+const student1 = new Student('Marek', 'Nowak')
+const student2 = new Student('Anna', 'Kowal')
 
-isBirthday(user)
-
-function isBirthday(user){
-    const today = getDate()
-    const userDate = user.born.day + '-' + user.born.month
-    if(today === userDate) {
-        console.log('Wszystkiego najlepszego!');
-    }else {
-        console.log('Nie masz dzis urodzin.');
-    }
-}
-
-function getDate(){
-    const date = new Date()
-    const day = date.getDate()
-    const month = date.getMonth() + 1
-    const formatMonth = month < 10 ? ('0' + month) : month
-    const today = day + "-" + formatMonth
-    return today
-}
+console.log(student1, student2);

d271dffd56bcd2fa525c8354c3a97932e1a5252f
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-19 16:36:31 +0200

    event cdn

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 const btnsList = document.querySelectorAll('button')
+const pElement = document.querySelector('p')
 
-const showObject = function(e){
-    console.log(e)
+const renderInfo = function(e){
+    if(pElement){
+        let text = parseInt(e.timeStamp / 1000)
+
+        text += ':' + this.tagName
+
+        text += ' -> ' + e.type
+
+        pElement.innerText = text
+    }
 }
 
 btnsList.forEach(function(item){
-    item.addEventListener('click', showObject)
+    item.addEventListener('click', renderInfo)
+    item.addEventListener('mouseenter', renderInfo)
 })

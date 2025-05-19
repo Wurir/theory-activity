00083cdf709f1c711411d46eb9207adca4264e7a
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-19 16:28:18 +0200

    capturing

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ const itemsList = document.querySelectorAll('section, article, h1')
 
 const showTagName = function(){
     console.log(this.tagName)
+    if(this.tagName === 'ARTICLE'){
+        this.removeEventListener('click', showTagName, true)
+    }
 }
 
 itemsList.forEach(function(item){
-    item.addEventListener('click', showTagName, false)
+    item.addEventListener('click', showTagName, true)
 })

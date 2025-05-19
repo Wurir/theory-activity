76ae0243839a5c066ad4dfe2b491d93e54fd6c9f
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-19 16:29:45 +0200

    capturing as default

--- js/app.js
+++ js/app.js
@@ -2,11 +2,8 @@ const itemsList = document.querySelectorAll('section, article, h1')
 
 const showTagName = function(){
     console.log(this.tagName)
-    if(this.tagName === 'ARTICLE'){
-        this.removeEventListener('click', showTagName, true)
-    }
 }
 
 itemsList.forEach(function(item){
-    item.addEventListener('click', showTagName, true)
+    item.addEventListener('mouseenter', showTagName)
 })

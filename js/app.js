53c7e59399a5e3d0358f62ac5588ee0b63967ce0
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-20 12:28:24 +0200

    e.stopImmediatePropagation()

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
-const itemsList = document.querySelectorAll('section, article, h1')
-
-const showTagName = function(e){
-    console.log(this.tagName)
-
-    if(this.tagName === 'H1'){
-        e.stopPropagation()
+const itemsList = document.querySelectorAll(
+    'section, article, h1'
+);
+const showTagName = function (e) {
+    console.log(this.tagName);
+    if (this.tagName === 'ARTICLE') {
+        e.stopImmediatePropagation();
     }
 }
-
-itemsList.forEach(function(item){
-    item.addEventListener('click', showTagName)
-})
+const showText = function () {
+    console.log('click on article!');
+}
+itemsList.forEach(function (item) {
+    item.addEventListener('click', showTagName);
+    if (item.tagName === 'ARTICLE') {
+        item.addEventListener('click', showText);
+    }
+});

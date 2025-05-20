ef53a9bf4597a5f037c0a95132ff0ff5b890a78e
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-20 14:38:05 +0200

    .click() | isTrusted

--- js/app.js
+++ js/app.js
@@ -1,15 +1,9 @@
-const itemsList = document.querySelectorAll(
-    'section, article, h1'
-);
-const showInfo = function (e) {
-    if(e.target === e.currentTarget){
-        const text = e.target.tagName
-        console.log(text + ' was clicked!')
+const btnElement = document.querySelector('button')
 
-    }
+const handleClick = function (e) {
+    console.log('button was clicked')
+    console.log(e.isTrusted)
 }
 
-itemsList.forEach(function (item) {
-    item.addEventListener('click', showInfo);
-
-});
+btnElement.addEventListener('click', handleClick)
+btnElement.click()

11a4cbaa0cba332fc0091a69ff10a3f8d629ae88
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-10-06 18:29:48 +0200

    addEventListener function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
+const handleClick = function(){
+    console.log('button was clicked')
+}
+
 const btnEl = document.querySelector('.btn')
 
 if(btnEl) {
     // jesli został wyszukany
 
-    btnEl.addEventListener('click', function(){
-        // wywolaj te funcke, jesli wyszukany przycisk zostanie klikniety
-
-        console.log('button was clicked')
-        // wyswietl napis w consoli
-    })
+    btnEl.addEventListener('click', handleClick)
 }

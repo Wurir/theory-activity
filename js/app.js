afc41cea828d39e0f2315e5281200c7e0fc1ba69
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-10-07 15:59:27 +0200

    "e"  getting info about event

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 const btnsList = document.querySelectorAll('button')
 
-const showInformation = function(e){
-    console.log(e.type, this.innerText)
+const showObject = function(e){
+    console.log(e)
+    // pobieram szczegolowe informacje
 }
 
 btnsList.forEach(function(btn){
-    btn.addEventListener('mouseenter', showInformation)
-    btn.addEventListener('click', showInformation)
+    btn.addEventListener('mouseenter', showObject)
+    btn.addEventListener('click', showObject)
 })

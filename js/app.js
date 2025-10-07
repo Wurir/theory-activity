76a34ef40cef488843612594b0a00657b24d3436
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-10-07 16:09:29 +0200

    using info from event

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 const btnsList = document.querySelectorAll('button')
+const pElement = document.querySelector('p')
 
-const showObject = function(e){
-    console.log(e)
-    // pobieram szczegolowe informacje
+const renderInfo = function(e){
+    if(pElement){
+        let text = parseInt(e.timeStamp / 1000)
+        // ilosc sekund of uruchomienia strony
+        text += ': ' + this.tagName
+        text += ' -> ' + e.type
+        pElement.innerText = text
+    }
 }
 
 btnsList.forEach(function(btn){
-    btn.addEventListener('mouseenter', showObject)
-    btn.addEventListener('click', showObject)
+    btn.addEventListener('mouseenter', renderInfo)
+    btn.addEventListener('click', renderInfo)
 })

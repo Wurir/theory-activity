e10abf9457da2cc41ada85bdd610dd63451db95f
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-10-07 15:23:26 +0200

    capturing cdn

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ const itemsList = document.querySelectorAll('section, article, h1')
 
 const showTagName = function () {
     console.log(this.tagName)
+    if(this.tagName === 'ARTICLE'){
+        this.removeEventListener('click', showTagName, true)
+        // usuwam nasluchiwanie z elementu ARTICLE
+    }
 }
 
 itemsList.forEach(function (item) {

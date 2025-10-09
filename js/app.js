ccdb5506825632001ed32451ef9e778a22f7bf44
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-10-09 16:20:30 +0200

    target vs currentTarget

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const itemsList = document.querySelectorAll('section, article, h1')
 
 const showInfo = function(e){
-    let text = e.currentTarget.tagName + ': '
-    // pobieram nazwe tagu elementu, ktory zostal klikniety
-    text += e.currentTarget === this ? "Y" : 'N'
-    console.log(text);
+    if(e.target === e.currentTarget){
+        //wykonaj tylko wtedy gdy callback jest uruchomiony bezposrednio
+        //czyli nie przez propagacje
+        let text = e.target.tagName
+        console.log(text + ' was clicked')
+    }
 }
 
 itemsList.forEach(function(item){

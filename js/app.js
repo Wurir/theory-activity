939d87dc9c32861787553b64e1fe5cc51cde8d99
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-10-09 16:16:17 +0200

    e.target

--- js/app.js
+++ js/app.js
@@ -1,23 +1,13 @@
 const itemsList = document.querySelectorAll('section, article, h1')
 
-const showTagName = function(e){
-    console.log(this.tagName)
-
-    if(this.tagName === 'ARTICLE'){
-        e.stopImmediatePropagation()
-        // przy uzyciu stopPropagation, wywola sie funcka showText, natomiast
-        // uzywajac stopImmediatePropagation, showText nie zostanie wykonane
-    }
-}
-
-const showText = function(){
-    console.log('click on article!')
+const showInfo = function(e){
+    let text = e.target.tagName + ': '
+    // pobieram nazwe tagu elementu, ktory zostal klikniety
+    text += this.tagName
+    console.log(text);
 }
 
 itemsList.forEach(function(item){
-    item.addEventListener('click', showTagName)
-    if(item.tagName === 'ARTICLE'){
-        item.addEventListener('click', showText)
-    }
+    item.addEventListener('click', showInfo)
 })
 

13826de96cc0cb7a68b16e756fd3068d5cfe9be2
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-03-24 12:59:23 +0100

    different form selectors

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-const itemsList = document.querySelectorAll(
-    'section, article, h1'
-)
-
-const handleOutput = function (e) {
-    console.log(this.tagName, e.type)
-}
-itemsList.forEach(function (item) {
-    item.addEventListener('output', handleOutput)
-    if (item.tagName === 'H1') {
-        const outputEvent = new CustomEvent(
-            'output', {
-            detail: item.tagName,
-            bubbles: false,
-        }
-        )
-        item.dispatchEvent(outputEvent)
-    }
-})
+const formByQuery = document.querySelector('form')
+const formByPosition = document.forms[0]
+const formByName = document.forms['forName']

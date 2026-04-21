44badb6ec91f0f2d6ac0a61c53e5e2e686fd3a22
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-04-21 14:03:51 +0200

    constructor params

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 class Person {
-    constructor(){
-        this.firstName = 'Bartek'
-        this.lastName = 'Nowak'
+    constructor(firstN, lastN){ //konstruktor moze przyjmowac parametry przekazywane podczas tworzenia obiektu
+        this.firstName = firstN
+        this.lastName = lastN
     }
 }
 
-const person1 = new Person()
-const person2 = new Person()
-const person3 = new Person()
+const person1 = new Person('Bartek', 'Nowak') //przekazuje parametry, ktore ląduja w konstruktorze
+const person2 = new Person('Jan', 'Kowalski')
+const person3 = new Person('Anna', 'Wybyła')
 
 console.log(
     person1, // Person {firstName: 'Bartek', lastName: 'Nowak'} 
-    person2, // Person {firstName: 'Bartek', lastName: 'Nowak'} 
+    person2, // Person {firstName: 'Jan', lastName: 'Kowalski'} 
     person1 === person2 //false
 );

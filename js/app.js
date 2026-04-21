170eca726c7de5defb4bb9dee7dbcdd52a1445c4
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-04-21 15:13:41 +0200

    constructor params cdn

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 class Person {
-    constructor(firstN, lastN){ //konstruktor moze przyjmowac parametry przekazywane podczas tworzenia obiektu
-        this.firstName = firstN
-        this.lastName = lastN
+    constructor({name, age = 0}){ //przyjelo się ze nazwy parametrow sa takie same jak nazwy wlasciwosci, ale nie jest to regułą
+        this.name = name
+        this.age = age
     }
 }
 
-const person1 = new Person('Bartek', 'Nowak') //przekazuje parametry, ktore ląduja w konstruktorze
-const person2 = new Person('Jan', 'Kowalski')
-const person3 = new Person('Anna', 'Wybyła')
+const person1 = new Person({name: 'Bartek', age : 32}) //można wykorzystać destrukturyzację, rest operator lub wartosci domyślne
+const person2 = new Person({name: 'Jan'})
+const person3 = new Person({name: 'Anna', age: 58})
 
 console.log(
-    person1, // Person {firstName: 'Bartek', lastName: 'Nowak'} 
-    person2, // Person {firstName: 'Jan', lastName: 'Kowalski'} 
+    person1, // Person {name: 'Bartek', age: 32} 
+    person2, // Person {name: 'Jan', age: 0} 
     person1 === person2 //false
 );

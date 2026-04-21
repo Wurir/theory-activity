bd0c864d5f88a00e4ac11197252cd4dff97985fe
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-04-21 15:14:54 +0200

    odwolywanie sie do wlasciwosci

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ const person2 = new Person({name: 'Jan'})
 const person3 = new Person({name: 'Anna', age: 58})
 
 console.log(
-    person1, // Person {name: 'Bartek', age: 32} 
-    person2, // Person {name: 'Jan', age: 0} 
-    person1 === person2 //false
-);
+    person1.name, // Bartek
+    person1['name'] // Bartek
+); // mozna uzyc obu sposobow

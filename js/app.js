36e961ee760e426f49229e47a979e6072488a164
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-04-21 13:34:50 +0200

    constructor in class

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 class Person {
-
+    constructor(){
+        this.firstName = 'Bartek'
+        this.lastName = 'Nowak'
+    }
 }
 
 const person1 = new Person()
@@ -7,6 +10,7 @@ const person2 = new Person()
 const person3 = new Person()
 
 console.log(
-    typeof person1, //object
+    person1, // Person {firstName: 'Bartek', lastName: 'Nowak'} 
+    person2, // Person {firstName: 'Bartek', lastName: 'Nowak'} 
     person1 === person2 //false
 );
